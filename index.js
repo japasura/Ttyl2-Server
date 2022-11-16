edaf2dc4337b5993bf8c5c93fe8b783d576e4a85
Samarth <samarthr1@outlook.com>
2022-11-16 16:28:50 +0530

    BUG: add in CORS support

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const dotenv = require("dotenv");
 dotenv.config();
 const url = process.env.MONGO_URI;
 
-var MongoClient = require("mongodb").MongoClient
+const MongoClient = require("mongodb").MongoClient;
 const express = require('express');
 const bodyParser = require('body-parser')
-var logger = require('morgan');
+const logger = require('morgan');
+const cors = require('cors');
+
 
 //Mongo DB Connect part
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    var dbo = db.db("Ttyl2");
+    const dbo = db.db("Ttyl2");
 //----------------------------
 
     const app = express()
@@ -21,6 +23,7 @@ MongoClient.connect(url, function(err, db) {
 
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended: true}))
+    app.use(cors())
 
     app.get('/', (req, res) => res.send('Hello World!'))
     app.get('/dayData/:date/events', function(req, res) {
